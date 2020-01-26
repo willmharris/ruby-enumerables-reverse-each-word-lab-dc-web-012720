db01226ae1d4423e2575a0cbf2a093eb662cabcb
William Harris <willmharris0@gmail.com>
2020-01-26 20:04:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   initial_array = string.split 
-  final_array = initial_array.map do 
-    |word| "#{word.reverse} "
+  final_array = initial_array.map do |word| 
+    "#{word.reverse} "
   end
   reversed_string = final_array.join
   reversed_string
