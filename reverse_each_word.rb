456826dc8249abee4812c667cb58beed1978e22c
William Harris <willmharris0@gmail.com>
2020-01-26 20:00:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  working_array = string.split 
-  working_array.map { |word| word.reverse}
-  # map working_array for each element to the element reversed 
-  reversed_string 
+  initial_array = string.split 
+  final_array = initial_array.map { |word| word.reverse}
+  final_array.join
+  final_array
 end 
